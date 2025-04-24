2e6cd26e4b00cb6cab27c6d73af307ebfcdc0f9a
Saurabh Kumar srivastwa <105786539+saurabhkumarsri@users.noreply.github.com>
2025-04-24 15:42:22 +0530

    Add files via upload

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 
 echo "Downloading script1.sh and script2.sh..."
 
-curl -O https://raw.githubusercontent.com/USERNAME/my-scripts/main/script1.sh
-curl -O https://raw.githubusercontent.com/USERNAME/my-scripts/main/script2.sh
+curl -O https://raw.githubusercontent.com/saurabhkumarsri/my-scripts/main/script1.sh
+curl -O https://raw.githubusercontent.com/saurabhkumarsri/my-scripts/main/script2.sh
 
 chmod +x script1.sh script2.sh
 
 echo "Running both scripts..."
-./script1.sh
-./script2.sh
+sudo ./script1.sh
+sudo ./script2.sh
 
